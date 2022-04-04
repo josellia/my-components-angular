961fb45be5c7f3fdb3535bc2e03b1c4d46dc3c9a
josellia <joseliaapires@hotmail.com>
2022-04-04 18:17:08 -0300

    style: card inicial

--- design-system/src/app/app.component.ts
+++ design-system/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'design-system';
+  public cards: Array<any> = [];
+
+  ngOnInit(): void {
+      this.cards = [
+        {
+          title: 'test',
+          content: 'bbbvbvbvbv'
+        },
+
+        {
+          title: 'test2',
+          content: 'bbbvbvbvbv'
+        }
+      ]
+  }
 }
